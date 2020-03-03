a5ebd2ddb6b1d0c3f219a41d99f611aec64394ed
voldemoriarty <mamin.bee16seecs@seecs.edu.pk>
2020-03-03 23:39:35 +0500

    Update doc

--- src/main/scala/RamCfg.scala
+++ src/main/scala/RamCfg.scala
@@ -1,9 +1,12 @@
 /**
  * Configuration for rams
- * @param nWords  number of words (depth)
- * @param nWrite  number of write ports
- * @param nBits   bits per word (width)
- * @param nRead   number of read ports
+ * @param nWords      number of words (depth)
+ * @param nWrite      number of write ports
+ * @param nBits       bits per word (width)
+ * @param nRead       number of read ports
+ * @param useRdEn     use a read enable signal in each read port
+ * @param asyncReads  use async read style
+ * @param mlabAttr    Add an attribute to force Quartus to infer MLABS. Useful with asyncReads
  */
 case class RamCfg(
                  nWords: Int = 32,
